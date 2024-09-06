1a8b5a8746d7f9c69f283b2d06f6b7b662b411ed
Breno L. Lopes <brernolopes@hotmail.com>
2024-09-06 10:07:56 -0300

    test: :test_tube: test
    Testa se a imagem é visivel, se está com a url e o alt corretos

--- cypress/e2e/mainPage.cy.js
+++ cypress/e2e/mainPage.cy.js
@@ -10,3 +10,25 @@ describe('Verifica a cor do background', () => {
       .should('have.css', 'background-color', 'rgb(255, 245, 250)')
   });
 });
+
+describe('Verifica se existe uma imagem', () => {
+  it('existe a tag image ".main-image"', () => {
+    cy.get('.main-image')
+      .should('exist')
+  });
+
+  it('A imagem não deve estár vazia', () => {
+    cy.get('.main-image')
+      .should('have.attr', 'src')
+      .and('not.be.empty');
+
+    cy.get('img').should('be.visible');
+  });
+
+  it('A tag deve conter um alt descritivo', () => {
+    Cy.get('.main-image')
+    .should('have.attr', 'alt')
+    .be('Omelete')
+  });
+});
+
