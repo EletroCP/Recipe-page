7e8c7db398e723da65c020a3a334622320d86ad4
Breno L. Lopes <brernolopes@hotmail.com>
2024-09-06 10:06:26 -0300

    test: :test_tube: teste
    Teste de coneção local

--- cypress/e2e/mainPage.cy.js
+++ cypress/e2e/mainPage.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Verifica se conecta a pagina', () => {
+  it('Deve carregar a pagina', () => {
     cy.visit('http://localhost:5500/')
-  })
-})
+  });
+});
+
